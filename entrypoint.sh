2df9285de1aa407562f1f7f7384a214c6b9a1939
Joosung Park <admin@slowmotion.dev>
2019-11-25 19:37:37 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,5 @@ if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
   cf target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
 fi
 
+export CF_DOCKER_PASSWORD = "$INPUT_CF_DOCKER_PASSWORD"
 sh -c "cf $*"
