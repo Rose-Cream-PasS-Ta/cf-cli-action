8253f1fd4639a2710af35219a09c62b4956f678a
Joosung Park <admin@slowmotion.dev>
2019-11-25 19:49:28 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/sh -l
-
 cf api --skip-ssl-validation "$INPUT_CF_API"
 cf auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
 
-if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
-  cf target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
-fi
-
-CF_DOCKER_PASSWORD = "$INPUT_CF_DOCKER_PASSWORD"
-export CF_DOCKER_PASSWORD
-
-sh -c "cf $*"
+bash -c "CF_DOCKER_PASSWORD=$INPUT_CF_DOCKER_PASSWORD cf $*"
